f7bbd024b226970184ebacdea3de59f8396a930e
Joshua Ricarder Oducado <31839332+projectvelox@users.noreply.github.com>
2018-10-04 10:19:23 +0800

    unavailable modal

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -5,6 +5,8 @@
 
 <body>
 	<?php include 'views/partials/navbar.php'?>
+	<?php include 'views/partials/unavailable.php'?>
+
 
 	<div class="container">
 		<h2>Admin Dashboard - Welcome <?php echo $_SESSION['account']['account_name'] ?></h2><hr>
@@ -13,16 +15,22 @@
 				<h3>Master Data</h3><br>
 				<div class="row">
 					<div class="col-xs-6 col-md-3 yccl-icon-rotation">
-						<img class="img-responsive" src="assets/images/icon-package.png">
-						<p class="yccl-icon-text"><strong>Add/Edit<br>Centers</strong></p>
+						<a data-toggle="modal" data-target="#unavailable">
+							<img class="img-responsive" src="assets/images/icon-package.png">
+							<p class="yccl-icon-text"><strong>Add/Edit<br>Centers</strong></p>
+						</a>
 					</div>
 					<div class="col-xs-6 col-md-3 yccl-icon-rotation">
-						<img class="img-responsive" src="assets/images/icon-package.png">
-						<p class="yccl-icon-text"><strong>Add/Edit<br>Doctors</strong></p>
+						<a data-toggle="modal" data-target="#unavailable">
+							<img class="img-responsive" src="assets/images/icon-package.png">
+							<p class="yccl-icon-text"><strong>Add/Edit<br>Doctors</strong></p>
+						</a>
 					</div>
 					<div class="col-xs-6 col-md-3 yccl-icon-rotation">
-						<img class="img-responsive" src="assets/images/icon-package.png">
-						<p class="yccl-icon-text"><strong>Add/Edit<br>Tests</strong></p>
+						<a data-toggle="modal" data-target="#unavailable">
+							<img class="img-responsive" src="assets/images/icon-package.png">
+							<p class="yccl-icon-text"><strong>Add/Edit<br>Tests</strong></p>
+						</a>
 					</div>
 					<div class="col-xs-6 col-md-3 yccl-icon-rotation">
 						<img class="img-responsive" src="assets/images/icon-package.png">
